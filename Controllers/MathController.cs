4ba56188d38f8fe86740c2c404612662ff63837a
Federico Cangiotti <101499329+FedericoCangiottiScuola@users.noreply.github.com>
2024-05-08 06:23:19 +0200

    feat: add MultiploIntero route in MathController

--- Controllers/MathController.cs
+++ Controllers/MathController.cs
@@ -2,5 +2,30 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AS2324_5G_INF_CangiottiFederico_API.Controllers
 {
-    
+    public class MathController : Controller
+    {
+        [HttpGet("GetDivisione")]
+
+        public JsonResult MultiploIntero(float firstNumber, float secondNumber)
+        {
+            string output = string.Empty;
+            string status = string.Empty;
+            string messagge = string.Empty;
+
+
+            if (firstNumber % secondNumber == 0)
+            {
+                output = "true";
+                status = "OK";
+                messagge = "The first number is multiple of the second number";
+            }
+
+            return new JsonResult(new
+            {
+                output = output,
+                status = status,
+                messagge = messagge
+            });
+        }
+    }
 }
